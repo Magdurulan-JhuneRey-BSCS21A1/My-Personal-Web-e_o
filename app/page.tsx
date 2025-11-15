edf9d96b8f3949e341f658fac8d0a1e762a3dcc2
Jhune Rey E. Magdurulan <mjhunerey28@gmail.com>
2025-11-15 20:58:56 +0800

    adjust left margin o_f Heavenly Cha_os

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-blue-700 to-purple-700 font-sans dark:bg-black">
       <Header />
-      <main className="flex flex-1 flex-col md:flex-row w-full max-w-3xl mx-auto px-4 pt-28 gap-8 items-center md:items-start">
-        <div className="flex-1 -ml-40">
+      <main className="flex flex-1 flex-col md:flex-row w-full max-w-3xl mx-auto px-4 pt-24 gap-8 items-center md:items-start">
+        <div className="flex-1 -ml-50">
           <h1 className="text-9xl md:text-8xl font-bold leading-tight text-white dark:text-zinc-50">
             Heavenly <br/>
             Cha_os
